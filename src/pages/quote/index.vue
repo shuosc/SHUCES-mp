d1778db1de9089851cf6f1220d52a6281e1e4a04
Dolinv <saichi_shaw@126.com>
2018-06-06 00:08:28 +0800

    quote changed

--- src/pages/quote/index.vue
+++ src/pages/quote/index.vue
@@ -3,77 +3,100 @@
     <div class="principle">
       <image src="/static/images/Guoyk.jpg"/>
     </div>
-    <blockquote>
-      <p class="quotation">
-        什么是真正的一流大学，在我看来，就是要凝聚一批学者，他们以为人类做出贡献、 产生让人类铭记的研究为目标。<br/>
-        有了这样一批学者当老师，继而把做科研的乐趣传递给年轻的学生， 让他们觉得青春时代受到很好的教育。——这样的大学就是一流大学。<br/>我们要为建设这样的大学而踏实奋斗！
-      </p>
-    </blockquote>
-    <div class="footer">——郭毅可院长</div>
+    <div class = "pic-note">上海大学计算机学院院长&emsp;郭毅可</div>
+    <div class = "line"></div>
+    <div class = "paragraph">
+      <div class = "title-block">
+        <image src = "/static/block.png"/>
+        <p>院长寄语</p>
+      </div>
+      <div class="quotation">
+        <p>什么是真正的一流大学?<br/></p>
+        <div class = "quotation"><br/></div>
+        <p>在我看来，<br/></p>
+        <p>就是要凝聚一批学者，<br/></p>
+        <p>他们以为人类做出贡献、<br/></p>
+        <p>产生让人类铭记的研究为目标。<br/></p>
+        <p>有了这样一批学者当老师，<br/></p>
+        <p>继而把做科研的乐趣<br/></p>
+        <p>传递给年轻的学生，<br/></p>
+        <p>让他们觉得青春时代受到很好的教育。<br/></p>
+        <div class = "quotation"><br/></div>
+        <p>——这样的大学就是一流大学。<br/></p>
+        <p>我们要为建设这样的大学而踏实奋斗！</p>
+      </div>
+    </div>
+    <div class = "line" ></div>
+    <div class = "quotation"><br/></div>
   </div>
 </template>
+
 <script>
-export default {}
+  export default {}
 </script>
+
 <style scoped>
-.page {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.principle {
+  .page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .principle {
+    width: 100%;
+    height: 1100rpx;
+  }
+  .principle image {
+    width: 100%;
+    height: 1100rpx;
+  }
+  .pic-note {
+    width: 100%;
+    margin-top: 28rpx;
+    margin-bottom: 28rpx;
+    text-align: center;
+    font-size: 30rpx;
+    font-family: 等线;
+  }
+  .line{
+    margin-top: 10rpx;
+    width:85%;
+    height: 2rpx;
+    background:#aaa;
+  }
+  .paragraph{
+    width: 100%;
+    margin: 40rpx 0rpx 5rpx 0rpx;
+    text-align: center;
+  }
+  .title-block{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: left;
+  }
+  .title-block image{
+    margin-left: 40rpx;
+    width: 45rpx;
+    height: 45rpx;
+  }
+  .title-block p{
+    font-size: 45rpx;
+    font-family: 黑体;
+    font-weight: bold;
+    line-height: 45rpx;
+    margin-left: 20rpx;
+  }
+  .quotation {
+    background-color:antiquewhite;
+    margin: 50rpx 40rpx 20rpx 40rpx;
+    /* margin: 0 auto; */
+    /* quotes: '\201C''\201D''\2018''\2019'; */
+  }
+  .quotation p{
+    text-align: left;
+    font-family: 宋体;
+    font-size: 33rpx;
+    line-height: 2.3;
+  }
 
-}
-.principle image {
-  height: 750rpx;
-}
-.remarks {
-  margin-top: 50rpx;
-  margin-left: 10%;
-  margin-right: 10%;
-  text-align: left;
-  font-family: 楷体;
-}
-.right {
-  text-align: right;
-}
-.quotation {
-  font-size: 15px;
-  border-left: 10px solid #ccc;
-  /* margin: 0 auto; */
-  quotes: '\201C''\201D''\2018''\2019';
-  padding: 10px 20px;
-  line-height: 1.4;
-  margin: 10px;
-}
-
-.quotation:before {
-  content: open-quote;
-  display: inline;
-  line-height: 0;
-  left: -10px;
-  position: relative;
-  top: 10px;
-  color: #ccc;
-  font-size: 3em;
-}
-.quotation::after {
-  content: close-quote;
-  display: inline;
-  line-height: 0;
-  left: 10px;
-  position: relative;
-  top: 15px;
-  color: #ccc;
-  font-size: 3em;
-}
-.footer {
-  display:block;
-  margin: 0;
-  width:100vw;
-  margin-right:20px;
-  text-align: right;
-  font-size: 20px;
-  font-style: italic;
-}
 </style>
